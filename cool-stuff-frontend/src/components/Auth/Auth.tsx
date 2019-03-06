8a71daa3bd1200c4b5410f7c19105f1b6f6993d4
sebstryczek <sebastian@stryczek.pl>
2019-03-07 00:18:41 +0100

    Create auth context

--- cool-stuff-frontend/src/components/Auth/Auth.tsx
+++ cool-stuff-frontend/src/components/Auth/Auth.tsx
@@ -1,9 +1,47 @@
-import React, { ReactNode } from 'react';
+import React, { createContext, ReactNode, Component } from 'react';
 
+/*
 type Props = {
   children({ isAuth }: {isAuth: Boolean}): ReactNode,
 };
+*/
 
+type Props = {
+  children: ReactNode,
+};
+
+type s = {
+  isAuthenticated: () => void,
+  signIn: () => void,
+  signOut: () => void,
+  signUp: () => void,
+};
+
+const AuthContext = createContext({
+  isAuthenticated: () => console.log('isAuthenticated'),
+  signIn: () => console.log('signIn'),
+  signOut: () => console.log('signOut'),
+  signUp: () => console.log('signUp'),
+});
+
+export const AuthConsumer = AuthContext.Consumer;
+
+export class AuthProvider extends Component {
+  authService: s;
+  constructor(props: Props) {
+    super(props);
+    this.authService = {
+      isAuthenticated: () => console.log('isAuthenticated'),
+      signIn: () => console.log('signIn'),
+      signOut: () => console.log('signOut'),
+      signUp: () => console.log('signUp'),
+    };
+  }
+  render() {
+    return <AuthContext.Provider value={this.authService}>{this.props.children}</AuthContext.Provider>;
+  }
+}
+/*
 const Auth = ({ children } : Props) => (
   <>
     {
@@ -15,3 +53,4 @@ const Auth = ({ children } : Props) => (
 );
 
 export default Auth;
+*/
